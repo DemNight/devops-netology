5440602cda1a51e6ecda38977a9bc5cebc239bf5
Boyarov <boyarov_io@mail.ru>
2021-11-05 15:19:25 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> cc6b8b9 (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
