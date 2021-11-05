cc06a77faa62ec6eea6d74aa344113a28e715b35
Boyarov <boyarov_io@mail.ru>
2021-11-05 15:21:11 +0300

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -4,10 +4,14 @@
 count=1
 for param in "$@"; do
 <<<<<<< HEAD
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
 =======
     echo "Parameter: $param"
 >>>>>>> cc6b8b9 (git-rebase 1)
+=======
+    echo "Next parameter: $param
+>>>>>>> f4a67e2 (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
